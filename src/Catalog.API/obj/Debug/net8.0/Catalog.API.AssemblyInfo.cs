1339717f048fd161a93479ecde3fd9a1a909711f
thanhhoa0000 <109255962+thanhhoa0000@users.noreply.github.com>
2024-09-25 08:58:06 +0000

    update cache files

--- src/Catalog.API/obj/Debug/net8.0/Catalog.API.AssemblyInfo.cs
+++ src/Catalog.API/obj/Debug/net8.0/Catalog.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d0c316f685155595670ced4b6c2756fc7d36364")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59f116faca1271ad05eb9b4a97cfe9d42ea1cdb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/TechnoTH.AppHost/obj/Debug/net8.0/TechnoTH.AppHost.AssemblyInfo.cs
+++ src/TechnoTH.AppHost/obj/Debug/net8.0/TechnoTH.AppHost.AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechnoTH.AppHost")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d0c316f685155595670ced4b6c2756fc7d36364")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59f116faca1271ad05eb9b4a97cfe9d42ea1cdb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechnoTH.AppHost")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechnoTH.AppHost")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
